45a0c45582b223cc51820d459556154e85a2388b
Bryan Kleinberg <bmklei8p@gmail.com>
2022-12-09 11:21:00 -0500

    playing around with landing page graphics/look/ui

--- ghi/src/CallToAction.js
+++ ghi/src/CallToAction.js
@@ -1,13 +1,22 @@
+import logo from "./media/temp.png"
 
 function CallToAction() {
     return (
         <>
-        {/* <div className="container"> */}
-            <h2 className="topCentered" style={{color: "white", fontSize: "36px"}}>Maybe start exploring by purchasing a car?</h2>
-            <img alt="red bull winning 2022 f1 car" src="https://pbs.twimg.com/media/FLKmB02WYAAUQuM?format=jpg&name=large"/>
-        {/* </div> */}
+            <div>
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <h2 className="topCentered" style={{color: "white", fontSize: "44px"}}>Maybe start exploring by purchasing a car?</h2>
+                <img className="img-fluid" alt="red bull winning 2022 f1 car" src={logo}/>
+            </div>
         </>
     )
 }
 
 export default CallToAction
+
+
+// "https://pbs.twimg.com/media/FLKmB02WYAAUQuM?format=jpg&name=large"
