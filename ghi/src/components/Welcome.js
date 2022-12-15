fc60959798dcb01af89e39a04b828c8545b7d650
Bryan Kleinberg <bmklei8p@gmail.com>
2022-12-15 14:26:46 -0500

    animation to start at left and move title to center complete

--- ghi/src/components/Welcome.js
+++ ghi/src/components/Welcome.js
@@ -6,23 +6,23 @@ function Welcome() {
     const [ trigger, setTrigger ] = useState(false)
 
     // This will cause a delay in the animation of 14 seconds to give time for the video
-    // to finish
+    // to finish  use 14000
     useEffect(() => {
         const timer = setTimeout(() => {
             setTrigger(true);
-        }, 14000);
+        }, 13000);
     }, []);
 
     return (
     <>
         {/* This will cause animation to trigger after the state is updated to true from the useEffect */}
         { trigger ? (
-            <motion.div animate={{x: 0}} transition={{ ease: "easeOut", duration: 2}} className="videoContent">
-                <h1 style={{fontSize: "110px"}}>Formula 1 Pit Box</h1>
-                <h2 style={{fontSize: "60px"}}> Your one-stop for all things Formula 1</h2>
-            </motion.div>
+            <div className="videoContent">
+                <motion.h1 animate={{x: 1000}} transition={{ ease: "easeOut", duration: 2}} style={{fontSize: "110px"}}>Formula 1 Pit Box</motion.h1>
+                <motion.h2 animate={{x: 950}} transition={{ ease: "easeOut", duration: 2}} style={{fontSize: "60px"}}> Your one-stop for all things Formula 1</motion.h2>
+            </div>
         ) : (
-            <p style={{display: "none"}}></p>
+            <></>
         )}
     </>
     )
